cc733bceea9842dded40fec9e14df95d752e31f9
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-12-31 11:49:12 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,40 +1,53 @@
+const debounce = (func, delay) => {
+  let timer;
+  return function () {
+    clearTimeout(timer);
+    timer = setTimeout(() => func.apply(this, arguments), delay);
+  };
+};
+
 const checkInterval = 100;
+const debouncedHandleYouTube = debounce(handleYouTube, checkInterval);
 
-function skipAds() {
-  const adOverlayCloseButton = document.querySelector(".ytp-ad-overlay-close-button");
-  const skipAdButton = document.querySelector(".ytp-ad-skip-button");
-  const incomingAdAlert = document.querySelector(".ytp-ad-message-container");
-
-  if (adOverlayCloseButton) {
-    adOverlayCloseButton.click();
-  }
-
-  if (skipAdButton) {
-    skipAdButton.click();
-  }
-
-  if (incomingAdAlert) {
-    incomingAdAlert.style.display = "none";
+function safeClick(element) {
+  if (element) {
+    try {
+      element.click();
+    } catch (error) {
+      console.error('Error clicking element:', error);
+    }
   }
 }
 
+function skipAds() {
+  const adOverlayCloseButton = document.querySelector(".ytp-ad-overlay-close-button");
+  const skipAdButton = document.querySelector(".ytp-ad-text.ytp-ad-skip-button-text");
+  const incomingAdAlert = document.querySelector(".ytp-ad-message-container");
+
+  safeClick(adOverlayCloseButton);
+  safeClick(skipAdButton);
+
+  incomingAdAlert?.remove();
+}
+
 function removeAds() {
-  const sideAds = document.querySelectorAll(".ytd-display-ad-renderer, .ytd-video-masthead-ad-renderer, .ytd-promoted-video-renderer");
-  const companionSlot = document.querySelector(".ytd-companion-slot-renderer");
-  const videoAds = document.querySelector(".video-ads.ytp-ad-module");
+  const sideAdsSelector = ".style-scope.ytd-watch-next-secondary-results-renderer.sparkles-light-cta.GoogleActiveViewElement, .style-scope.ytd-item-section-renderer.sparkles-light-cta";
+  const sideAds = document.querySelectorAll(sideAdsSelector);
 
   sideAds.forEach(sideAd => sideAd.style.display = "none");
 
-  if (companionSlot) {
-    companionSlot.remove();
-  }
+  const companionSlot = document.querySelector(".style-scope.ytd-companion-slot-renderer");
+  const videoAds = document.querySelector(".video-ads.ytp-ad-module");
+
+  companionSlot?.remove();
 
   if (videoAds && videoAds.children.length > 0) {
-    videoAds.querySelectorAll('.ytp-ad-module').forEach(ad => ad.remove());
+    const previewText = document.querySelector(".ytp-ad-text.ytp-ad-preview-text");
     const videoElement = document.querySelector(".video-stream.html5-main-video");
 
     if (previewText) {
-      videoElement.playbackRate = 16;
+      const playbackRateForPreviewText = 16;
+      videoElement.playbackRate = playbackRateForPreviewText;
     }
   }
 }
@@ -44,4 +57,7 @@ function handleYouTube() {
   removeAds();
 }
 
-setInterval(handleYouTube, checkInterval);
+const observer = new MutationObserver(debouncedHandleYouTube);
+observer.observe(document.body, { childList: true, subtree: true });
+
+handleYouTube();  // Initial execution
